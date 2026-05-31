f722b2a0f919918f42b368a332514e5846e39200
Pownraj Sivaraj <Pownraj.Sivaraj@gds.ey.com>
2026-05-31 22:44:39 +0530

    Intial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<User>> GetUserById(string id)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(user => user.Id.Equals(id));
+
+            if (user == null) return NotFound();
+
+            return Ok(user);
+        }
+    }
+}
